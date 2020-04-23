64f45ec1d0f469bc9dec0df8cd192ed077277d83
AlekseyVolok <57563824+AlekseyVolok@users.noreply.github.com>
2020-04-23 22:41:02 +0200

    Dodano logowanie i ikonę

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
+const logger = require('koa-static')
+const favicon = require('koa-favicon')
+
 
 const app = new Koa()
 const port = process.env.PORT || 3000
+
+app.use(favicon('./client/favicon.ico'))
+app.use(logger())
 
 app.use(serve(path.resolve(__dirname, '..', 'client')))
 
